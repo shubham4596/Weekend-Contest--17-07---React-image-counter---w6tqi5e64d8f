ac694489c0eef020b664b2f7e595ac16eeed29ff
shubham4596 <104636433+shubham4596@users.noreply.github.com>
2022-07-17 12:55:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const [height, setHeight]= useState(300);
+  const [width, setWidth]= useState(300);
+  const increaseFunction= ()=>{
+    setHeight(height+2);
+    setWidth(width+2);
+  }
   return (
     <div id="main">
       <img src={star}  height="300px" width="300px" />
